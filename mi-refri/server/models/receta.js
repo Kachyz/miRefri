59a118ebd364c49c8ef2833b255e7e2a4ca93f1a
Kachyz <kachiiz@gmail.com>
2018-08-31 01:18:30 -0500

    adding ingredientes to the DB and the manipulation it has with recetas

--- mi-refri/server/models/receta.js
+++ mi-refri/server/models/receta.js
@@ -27,12 +27,31 @@ module.exports = function(Receta) {
 
     Receta.observe('after save', function(ctx, next){
       console.log(ctx.instance)
+      let newRecipe = ctx.instance
       // ctx.intance.ingredientes //array
       //Account.find({where: {name: 'John'}, limit: 3}, function(err, accounts) { /* ... */ });
 
-      app.models.Receta.find({where: {id: '5b88a065f1ed047281533ecc'}}, function(err, receta){
-        console.log('Desde el observe', receta)
-      })
+      // app.models.Receta.find({where: {id: '5b88a065f1ed047281533ecc'}}, function(err, receta){
+      //   console.log('Desde el observe', receta)
+      // })
+      for(let ingrediente of newRecipe.ingredientes){
+        app.models.ingrediente.findOrCreate(
+          {where: {nombre: ingrediente.name}}, 
+          {
+            "nombre": ingrediente.name,
+            "recetas": [newRecipe.id]
+          },
+          (err, instance, created) => {
+            if(!created){
+              instance.recetas.push(newRecipe.id)
+              instance.save((err, instance) => {
+                console.log('Ingrediente actualizado', instance)
+              }
+              )
+            }
+          }
+        )
+      }
 
       next();
     })
